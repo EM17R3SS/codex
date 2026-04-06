ecc9f2241267ddf29ff598360e87d044b4e51624
EM17R3SS <fertok.fex@gmail.com>
2026-04-06 23:35:40 +0300

    edits 1 2 3

--- src/TXTAdder/script.js
+++ src/TXTAdder/script.js
@@ -1,5 +1,7 @@
-const fs = require("fs").promises;
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require("path");
+const readLine = require("readline");
 
 async function TXTAdder() {
     try {
@@ -8,16 +10,51 @@ async function TXTAdder() {
             path.join(__dirname, "text2.txt"),
             path.join(__dirname, "text3.txt"),
         ];
-        const contents = await Promise.all(
-            FilePath.map((filePath) => fs.readFile(filePath, "utf8")),
-        );
-        const combo = contents.join("\n");
         const outputFile = path.join(__dirname, "output.txt");
-        const cnt = combo.split("\n").length;
+        const writeStream = fs.createWriteStream(outputFile, {
+            encoding: "utf8",
+        });
+        let totalLines = 0;
 
-        await fs.writeFile(outputFile, combo, "utf8");
+        for (let i = 0; i < FilePath.length; i++) {
+            const filePath = FilePath[i];
+            const readStream = fs.createReadStream(filePath, {
+                encoding: "utf8",
+            });
+            const reli = readLine.createInterface({
+                input: readStream,
+                crlfDelay: Infinity,
+            });
 
-        console.log(`DONE, count of strings = ${cnt}`);
+            let isFirstLine = true;
+
+            for await (const line of reli) {
+                if (!isFirstLine) {
+                    writeStream.write("\n");
+                }
+                writeStream.write(line);
+                isFirstLine = false;
+                if (line.trim() !== "") {
+                    totalLines++;
+                }
+            }
+            if (i < FilePath.length - 1 && !isFirstLine) {
+                writeStream.write("\n");
+            }
+        }
+        writeStream.end();
+        await new Promise((resolve) => writeStream.on("finish", resolve));
+        // const contents = await Promise.all(
+        //     FilePath.map((filePath) => fs.readFile(filePath, "utf8")),
+        // );
+        // const combo = contents.join("\n");
+        // // const outputFile = path.join(__dirname, "output.txt");
+        // const cnt = combo.trimEnd().split(/\r?\n|\r/).length;
+        // const lines = combo.trimEnd().split("\n");
+        // const cnt = lines.length;
+        //
+        // await fs.writeFile(outputFile, combo, "utf8");
+        console.log(`DONE, count of strings = ${totalLines}`);
     } catch (error) {
         console.error(error);
     }
